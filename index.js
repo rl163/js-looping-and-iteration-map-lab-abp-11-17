49ae13b64f7f39d77d066b9fa27bdf56f042664d
Ryan Lee <ryanlee91@gmail.com>
2017-11-17 04:48:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function nameToAttributes(names){
   // names = ['Bobby Smith', 'Sammy Watkins']
   names.map(function(name){
     //name 'bobby smith'
-    let fName = name.split(" ")[0];
-    let lName = name.split(" ")[1];
+    let fName = name.split(" ")[0]
+    let lName = name.split(" ")[1]
     return {firstName: fName, lastName: lName}
     // {firstName: 'Bobby', lastName: 'Smith'}
   })
